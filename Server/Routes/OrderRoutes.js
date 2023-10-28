d21f8ecfa801b24e773d56117928a7759e6d0b54
srikar1980 <srikar.ravoori@gmail.com>
2023-10-28 21:20:26 +0530

    later server side code is pused

---
+++ Server/Routes/OrderRoutes.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import protect from '../Middleware/AuthMiddleware.js';
+import Order from '../Models/OrderModel.js';
+
+const orderRouter = express.Router();
+// CREATE ORDER
+orderRouter.post(
+  '/',
+  protect,
+  asyncHandler(async (req, res) => {
+    const {
+      orderItems,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+      res.status(400);
+      throw new Error('No order items');
+      return;
+    } else {
+      const order = new Order({
+        orderItems,
+        user: req.user._id,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+      });
+
+      const createOrder = await order.save();
+      res.status(201).json(createOrder);
+    }
+  })
+);
+
+// GET ORDER BY ID
+orderRouter.get(
+  '/:id',
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'name email'
+    );
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error('Order not found!');
+    }
+  })
+);
+export default orderRouter;
